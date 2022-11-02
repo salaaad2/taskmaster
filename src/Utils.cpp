2d47bb6b47376cc4458b8d704ea7955a93c2c866
salaaad2 <arthurdurant263@gmail.com>
2022-11-02 23:06:40 +0100

    StringUtils redundant, move to Utils.{c,h}pp

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,11 +1,34 @@
 #include "Utils.hpp"
 
-void Utils::PrintError(const string & source, const string & reason)
+namespace Utils {
+
+void PrintError(const string & source, const string & reason)
 {
     std::cout << "taskmaster: error: " << source << ": " << reason << std::endl;
 }
 
-void Utils::PrintSuccess(const string & source, const string & reason)
+void PrintSuccess(const string & source, const string & reason)
 {
     std::cout << "taskmaster: success: " << source << ": " << reason << std::endl;
+}
+
+/*
+** return string vector split using
+** separator
+ */
+std::vector<std::string> SplitString(
+    std::string source,
+    const std::string &separator)
+{
+    std::vector<std::string> out;
+    int last_separator_pos = 0;
+
+    while (source.find(separator) != std::string::npos)
+    {
+        out.push_back(source.substr(last_separator_pos, source.find(separator)));
+        source.erase(0, source.find(separator) + separator.length());
+    }
+    return out;
+}
+
 }
--- src/Utils.hpp
+++ src/Utils.hpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <vector>
 
 using std::string;
 
 namespace Utils {
-    void PrintError(const string & source, const string & reason);
-    void PrintSuccess(const string & source, const string & reason);
+    void PrintError(
+        const string & source,
+        const string & reason);
+    void PrintSuccess(
+        const string & source,
+        const string & reason);
+    std::vector<std::string> SplitString(
+        std::string source,
+        const std::string &separator);
+
+    /*
+    ** with source a container with begin and end(),
+    ** return a const char * vector to be used by
+    ** C functions which require char*const*
+     */
+    template <typename T>
+    std::vector<const char*> JoinStrings(
+            const std::string & name,
+            const T & source)
+    {
+        std::vector<const char*> out;
+
+        out.push_back(name.c_str());
+        for (auto it = source.begin(); it != source.end(); ++it)
+        {
+            out.push_back((*it).c_str());
+        }
+        out.push_back(NULL);
+        return out;
+    }
 };
